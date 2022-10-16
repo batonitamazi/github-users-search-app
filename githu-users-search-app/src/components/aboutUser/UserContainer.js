326c4c821f170b29f5a4a7355eb24f634b151745
batonitamazi <tamazi.mirianashvili.1@btu.edu.ge>
2022-10-16 12:58:45 +0400

    feat: responsive design

--- githu-users-search-app/src/components/aboutUser/UserContainer.js
+++ githu-users-search-app/src/components/aboutUser/UserContainer.js
@@ -25,71 +25,76 @@ function UserContainer({ data }) {
     <>
       {data && (
         <div className={darkMode ? 'info--container info--container-dark' : 'info--container info--container-light'}>
-          <img src={data.avatar_url} alt='avatar' id='avatar' className='avatar--image' />
           <div className='user--info'>
             <div className='user--info-header'>
-              <div className='sub--header'>
-                <h1 className={darkMode ? 'header-light' : 'header-dark'}>{data.name}</h1>
-                <p className={darkMode ? 'header--paragraph header--paragraph-dark' : 'header--paragraph header--paragraph-light'}
-                >
-                  Joined {day} {months[month-1]} {year}
-                </p>
+              <img src={data.avatar_url} alt='avatar' id='avatar' className='avatar--image' />
+              <div className='parent--subheader'>
+                <div className='sub--header'>
+                  <h1 className={darkMode ? 'header-light' : 'header-dark'}>{data.name}</h1>
+                  <p className={darkMode ? 'header--paragraph header--paragraph-dark' : 'header--paragraph header--paragraph-light'}
+                  >
+                    Joined {day} {months[month - 1]} {year}
+                  </p>
+                </div>
+                <p className='user--link'>@{data.login}</p>
               </div>
-              <p className='user--link'>@{data.login}</p>
+            </div>
+            <div className='second--information'>
               <p className={darkMode ? 'header--paragraph header--paragraph-dark' : 'header--paragraph header--paragraph-light'}>
                 {data.bio}
               </p>
+              <div className={darkMode ? 'general--info-card general--info-card-dark' : 'general--info-card general--info-card-light'}>
+                <div className='sub--info'>
+                  <p className={darkMode ? 'sub--info-par sub--info-par-dark' : 'sub--info-par sub--info-par-light'}>
+                    Repos
+                  </p>
+                  <h4 className={darkMode ? 'sub--info-head sub--info-head-dark' : 'sub--info-head sub--info-head-light'}>{data.public_repos}</h4>
+                </div>
+                <div className='sub--info'>
+                  <p className={darkMode ? 'sub--info-par sub--info-par-dark' : 'sub--info-par sub--info-par-light'}>
+                    Followers
+                  </p>
+                  <h4 className={darkMode ? 'sub--info-head sub--info-head-dark' : 'sub--info-head sub--info-head-light'}>{data.followers}</h4>
+                </div>
+                <div className='sub--info'>
+                  <p className={darkMode ? 'sub--info-par sub--info-par-dark' : 'sub--info-par sub--info-par-light'}>
+                    Following
+                  </p>
+                  <h4 className={darkMode ? 'sub--info-head sub--info-head-dark' : 'sub--info-head sub--info-head-light'}>{data.following}</h4>
+                </div>
+              </div>
+              <div className='extra--info-card'>
+                <div className='extra--subinfo'>
+                  <div className='mini--info'>
+                    <img src='/assets/icon-location.svg' alt='location' className='' />
+                    <p className={darkMode ? 'header--paragraph header--paragraph-dark' : 'header--paragraph header--paragraph-light'}>
+                      {data.location || 'not available'}
+                    </p>
+                  </div>
+                  <div className='mini--info'>
+                    <img src='/assets/icon-website.svg' alt='website' />
+                    <p className={darkMode ? 'header--paragraph header--paragraph-dark' : 'header--paragraph header--paragraph-light'}>
+                      {data.blog || 'not available'}
+                    </p>
+                  </div>
+                </div>
+                <div className='extra--subinfo'>
+                  <div className='mini--info'>
+                    <img src='/assets/icon-twitter.svg' alt='twitter' />
+                    <p className={darkMode ? 'header--paragraph header--paragraph-dark' : 'header--paragraph header--paragraph-light'}>
+                      {data.twitter || 'not available'}
+                    </p>
+                  </div>
+                  <div className='mini--info'>
+                    <img src='/assets/icon-company.svg' alt='company' className={darkMode ? 'toggle--dark-img' : 'toggle-light-img'} />
+                    <p className={darkMode ? 'header--paragraph header--paragraph-dark' : 'header--paragraph header--paragraph-light'}>
+                      {data.company || 'not available'}
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className={darkMode ? 'general--info-card general--info-card-dark' : 'general--info-card general--info-card-light'}>
-              <div className='sub--info'>
-                <p className={darkMode ? 'sub--info-par sub--info-par-dark' : 'sub--info-par sub--info-par-light'}>
-                  Repos
-                </p>
-                <h4 className={darkMode ? 'sub--info-head sub--info-head-dark' : 'sub--info-head sub--info-head-light'}>{data.public_repos}</h4>
-              </div>
-              <div className='sub--info'>
-                <p className={darkMode ? 'sub--info-par sub--info-par-dark' : 'sub--info-par sub--info-par-light'}>
-                  Followers
-                </p>
-                <h4 className={darkMode ? 'sub--info-head sub--info-head-dark' : 'sub--info-head sub--info-head-light'}>{data.followers}</h4>
-              </div>
-              <div className='sub--info'>
-                <p className={darkMode ? 'sub--info-par sub--info-par-dark' : 'sub--info-par sub--info-par-light'}>
-                  Following
-                </p>
-                <h4 className={darkMode ? 'sub--info-head sub--info-head-dark' : 'sub--info-head sub--info-head-light'}>{data.following}</h4>
-              </div>
-            </div>
-            <div className='extra--info-card'>
-              <div className='extra--subinfo'>
-                <div className='mini--info'>
-                  <img src='/assets/icon-location.svg' alt='location' className='' />
-                  <p className={darkMode ? 'header--paragraph header--paragraph-dark' : 'header--paragraph header--paragraph-light'}>
-                    {data.location || 'not available'}
-                  </p>
-                </div>
-                <div className='mini--info'>
-                  <img src='/assets/icon-website.svg' alt='website' />
-                  <p className={darkMode ? 'header--paragraph header--paragraph-dark' : 'header--paragraph header--paragraph-light'}>
-                    {data.blog || 'not available'}
-                  </p>
-                </div>
-              </div>
-              <div className='extra--subinfo'>
-                <div className='mini--info'>
-                  <img src='/assets/icon-twitter.svg' alt='twitter' />
-                  <p className={darkMode ? 'header--paragraph header--paragraph-dark' : 'header--paragraph header--paragraph-light'}>
-                    {data.twitter || 'not available'}
-                  </p>
-                </div>
-                <div className='mini--info'>
-                  <img src='/assets/icon-company.svg' alt='company' className={darkMode ? 'toggle--dark-img' : 'toggle-light-img'} />
-                  <p className={darkMode ? 'header--paragraph header--paragraph-dark' : 'header--paragraph header--paragraph-light'}>
-                    {data.company || 'not available'}
-                  </p>
-                </div>
-              </div>
-            </div>
+
           </div>
         </div>
       )
